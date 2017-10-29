e8526697de5e7054e6e1c34941ce6e449e5ac850
ska95aks <31956125+ska95aks@users.noreply.github.com>
2017-10-30 00:14:01 +0900

    day13_hw

---
+++ day13_hw.py
@@ -0,0 +1,17 @@
+import turtle as t
+t. shape("turtle")
+t.up()
+t.fd(250)
+t.lt(90)
+t.fd(250)
+t.lt(90)
+t.down()
+for x in range(4):
+    t.fd(500)
+    t.lt(90)
+t.up()
+t.home()
+t.down()
+while True:
+  t.sety(250)
+  t.sety(-250)
